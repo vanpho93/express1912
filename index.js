ed8e80a770e89f2a630eff55c19771782ad070f3
Admin <vanpho02@gmail.com>
2017-01-02 20:55:21 +0700

    start

---
+++ index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+app.listen(3000);
+
+//route
+// app.get('/abc', function(request, response){
+//   response.send('Hello')
+// });
+
+// app.get('/abc', function(req, res){
+//   res.send('Hello');
+// });
+
+app.get('/', indexFunction);
+
+function indexFunction(req, res){
+  res.send(`<h1>Hello index route</h1>`);
+}
+
+app.get('/tinh/:pheptinh/:soa/:sob', function(req, res){
+  var {pheptinh, soa, sob} = req.params;
+  var pt = new PhepTinh(pheptinh, soa, sob);
+  res.send(pt.getOutput());
+});
+
+// function xuly(req, res){
+//   var {pheptinh, soa, sob} = req.params;
+//   var dau = ' % ';
+//   switch (pheptinh) {
+//     case 'cong':
+//       dau = ' + ';
+//       break;
+//     case 'tru':
+//       dau = ' - ';
+//       break;
+//     case 'nhan':
+//       dau = ' * ';
+//   }
+//   var strPhepTinh = soa + dau + sob;
+//   var kq = eval(strPhepTinh);
+//   res.send(`${strPhepTinh} = ${kq}`);
+// }
+
+app.get('api/:ten/:tuoi', function(req, res){
+  var {ten, tuoi} = req.params;
+  res.send(`Xin chao ban ${ten}: ${tuoi}`);
+});
+
+console.log(eval('2+3'));
+
+class PhepTinh {
+  constructor(pt, soa, sob){
+    this.pt = pt;
+    this.soa = soa;
+    this.sob = sob;
+  }
+  getOutput(){
+    var {pt, soa, sob} = this;
+    var dau = ' % ';
+    switch (pt) {
+      case 'cong':
+        dau = ' + ';
+        break;
+      case 'tru':
+        dau = ' - ';
+        break;
+      case 'nhan':
+        dau = ' * ';
+    }
+    var strPhepTinh = soa + dau + sob;
+    var kq = eval(strPhepTinh);
+    return strPhepTinh + ' = ' + kq;
+  }
+}
