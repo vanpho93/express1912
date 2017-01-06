59fff9390e3ddea4d567ea51caa9a85291a5243c
Admin <vanpho02@gmail.com>
2017-01-06 20:29:28 +0700

    xong

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(require('./controller/midleware.js'));
+
 app.listen(3000, () => console.log('Server started'));
 
 app.set('view engine', 'ejs');
